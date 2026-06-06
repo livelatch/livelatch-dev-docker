4b12bd313f8352cfd3622c7acf2d02d5fd183edb
alexkirwan29 <alexkirwan29@gmail.com>
2026-06-06 18:04:43 +0800

    Add Poppins typography and set APP_NAME
    Introduce a dedicated typography.css with Poppins font rules (body, headings, buttons, inputs, utility weights) and import it into app.css; define --font-primary and apply font-family/weights site-wide. Update layouts/app.blade.php to preconnect and load the Poppins Google Font. Also change APP_NAME in .env from "LinkStack" to "livelatch".

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,10 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="//fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
